6cde0fcd909d46dc284d1a197456513e6ef0b1d5
farrukh90 <farrukhsadykov@gmail.com>
2019-12-18 22:17:21 -0600

    added resource group

--- vm.tf
+++ vm.tf
@@ -2,11 +2,6 @@ variable "prefix" {
   default = "tfvmex"
 }
 
-resource "azurerm_resource_group" "main" {
-  name     = "${var.prefix}-resources"
-  location = "West US 2"
-}
-
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
